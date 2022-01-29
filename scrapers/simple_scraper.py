bea3beb87d705e9146839039b8e63aedf8ff1dda
Sam Garcia <samuelcgarcia.3@gmail.com>
2022-01-29 13:50:03 -0500

    commit basic web scraper

---
+++ scrapers/simple_scraper.py
@@ -0,0 +1,7 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+res = requests.get("https://en.wikipedia.org/wiki/Python_(programming_language)")
+bs = BeautifulSoup(res.text, 'lxml')
+print(bs.find("p", class_=None).text)
