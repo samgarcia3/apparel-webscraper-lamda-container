4b8e130fe305e71f5ff613ed740f3f950177a7e6
Sam Garcia <samuelcgarcia.3@gmail.com>
2022-01-29 14:42:24 -0500

    Optimizing the Selectors

--- scrapers/test_scrape.py
+++ scrapers/test_scrape.py
@@ -6,6 +6,10 @@ response = requests.get(url_to_parse)
 print(response)
 response_text = response.text
 soup = BeautifulSoup(response_text, 'lxml')
-toc = soup.find_all("span",class_="toctext")
-for item in toc:
-    print(item)
+
+all_main_headings = []
+main_headings = soup.find_all("li", class_="toclevel-1")
+for h1 in main_headings:
+    heading = h1.find("span", class_="toctext").text
+    all_main_headings.append(heading)
+    print(heading)
