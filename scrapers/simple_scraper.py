9d054c808f9284a2cf4e01a371371910c4b31b93
Sam Garcia <samuelcgarcia.3@gmail.com>
2022-01-29 13:53:44 -0500

    init

--- scrapers/simple_scraper.py
+++ scrapers/simple_scraper.py
@@ -5,3 +5,5 @@ from bs4 import BeautifulSoup
 res = requests.get("https://en.wikipedia.org/wiki/Python_(programming_language)")
 bs = BeautifulSoup(res.text, 'lxml')
 print(bs.find("p", class_=None).text)
+
+#test
